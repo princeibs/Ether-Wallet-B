658568b03ce167793fb8378898cad2bfb1c0ce2e
OAMEN PAUL <pauloams16@gmail.com>
2022-05-16 01:27:49 -0700

    update to ether-wallet-b

--- EtherWallet.sol
+++ EtherWallet.sol
@@ -1,17 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract EtherWallet {
     address payable public owner;
+    mapping(address=>uint) balances;
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    receive() external payable {}
+    function deposit() external payable {
+        balances[msg.sender] = msg.value;
+    }
 
     function withdraw(uint _amount) external {
-        require(msg.sender == owner, "caller is not owner");
+        // require(msg.sender == owner, "caller is not owner");
+        require(balances[msg.sender]>=_amount,"insufficient funds");
+        balances[msg.sender]-=_amount;
         payable(msg.sender).transfer(_amount);
     }
 
